17ae3e8ea01e5f2111f8e0530cfbb0cc5bb044a2
Trevor Goodell <tgoodell6@gatech.edu>
2022-10-09 19:09:05 -0400

    test

--- lib/dps-sim-backend-stack.ts
+++ lib/dps-sim-backend-stack.ts
@@ -1,16 +1,16 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Duration } from 'aws-cdk-lib';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class DpsSimBackendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'DpsSimBackendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const revisionQueue = new sqs.Queue(this, 'my-test-queue', {
+      queueName: 'MyTestQueue',
+      receiveMessageWaitTime: Duration.seconds(20)
+    });
   }
 }
